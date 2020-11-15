c60b9b0ffe0ffb36845fa1a7a5c9d341cfc8ba18
Ben Williams <benryanwilliams@hotmail.com>
2020-11-15 16:41:06 +0700

    Initial commit

---
+++ Cragbook/Controllers/Core Tabs/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cragbook
+//
+//  Created by Ben Williams on 15/11/2020.
+//  Copyright © 2020 Ben Williams. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
